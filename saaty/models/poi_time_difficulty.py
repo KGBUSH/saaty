b418338752a3259a7d6a3c29aff75bb11e72e5fc
zhuangxk <549223238@qq.com>
2018-04-11 19:53:20 +0800

    join index added

--- saaty/models/poi_time_difficulty.py
+++ saaty/models/poi_time_difficulty.py
@@ -11,14 +11,14 @@ class POISupplierTimeDifficulty(Model):
     __table_args__ = {"mysql_engine": "InnoDB", "mysql_charset": "utf8"}
     __bind_key__ = "saaty"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)    # 主键ID
-    supplier_id = db.Column(db.BigInteger)                          # 商户ID
-    supplier_lng = db.Column(db.VARCHAR, index=True, nullable=False)  # 商户的经度
-    supplier_lat = db.Column(db.VARCHAR, index=True, nullable=False)  # 商户的纬度
-    city_id = db.Column(db.Integer)                                 # 城市ID
-    poi_value = db.Column(db.Float)                                 # 难度值
-    create_time = db.Column(db.TIMESTAMP)                           #
-    update_time = db.Column(db.TIMESTAMP)                           #
+    id = db.Column(db.Integer, primary_key=True, nullable=False)        # 主键ID
+    supplier_id = db.Column(db.BigInteger)                              # 商户ID
+    supplier_lng = db.Column(db.VARCHAR, index=True, nullable=False)    # 商户的经度
+    supplier_lat = db.Column(db.VARCHAR, index=True, nullable=False)    # 商户的纬度
+    city_id = db.Column(db.Integer)                                     # 城市ID
+    poi_value = db.Column(db.Float)                                     # 难度值
+    create_time = db.Column(db.TIMESTAMP)                               #
+    update_time = db.Column(db.TIMESTAMP)                               #
 
 
 class POIReceiverTimeDifficulty(Model):
@@ -26,13 +26,13 @@ class POIReceiverTimeDifficulty(Model):
     __table_args__ = {"mysql_engine": "InnoDB", "mysql_charset": "utf8"}
     __bind_key__ = "saaty"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)    # 主键ID
-    receiver_lng = db.Column(db.VARCHAR, index=True, nullable=False)  # 收货地的经度
-    receiver_lat = db.Column(db.VARCHAR, index=True, nullable=False)  # 收货地的纬度
-    city_id = db.Column(db.Integer)                                 # 城市ID
-    poi_value = db.Column(db.Float)                                 # 难度值
-    create_time = db.Column(db.TIMESTAMP)                           #
-    update_time = db.Column(db.TIMESTAMP)                           #
+    id = db.Column(db.Integer, primary_key=True, nullable=False)        # 主键ID
+    receiver_lng = db.Column(db.VARCHAR, index=True, nullable=False)    # 收货地的经度
+    receiver_lat = db.Column(db.VARCHAR, index=True, nullable=False)    # 收货地的纬度
+    city_id = db.Column(db.Integer)                                     # 城市ID
+    poi_value = db.Column(db.Float)                                     # 难度值
+    create_time = db.Column(db.TIMESTAMP)                               #
+    update_time = db.Column(db.TIMESTAMP)                               #
 
 
 if __name__ == "__main__":
@@ -40,13 +40,16 @@ if __name__ == "__main__":
     supplier_id = 3665730
     supplier_lng = 121.32576
     supplier_lat = 31.46398
+    city_id = 1
+
 
     res = POISupplierTimeDifficulty.query \
         .filter(POISupplierTimeDifficulty.supplier_id == supplier_id,
                 POISupplierTimeDifficulty.supplier_lng == supplier_lng,
-                POISupplierTimeDifficulty.supplier_lat == supplier_lat) \
+                POISupplierTimeDifficulty.supplier_lat == supplier_lat,
+                POISupplierTimeDifficulty.city_id == city_id) \
         .first()
-    print(res)
+    print(res.poi_value)
 
 
     receiver_lng = 121.426364
@@ -54,6 +57,7 @@ if __name__ == "__main__":
 
     res = POIReceiverTimeDifficulty.query \
         .filter(POIReceiverTimeDifficulty.receiver_lng == receiver_lng,
-                POIReceiverTimeDifficulty.receiver_lat == receiver_lat) \
+                POIReceiverTimeDifficulty.receiver_lat == receiver_lat,
+                POIReceiverTimeDifficulty.city_id == city_id) \
         .first()
-    print(res[0].poi_value)
+    print(res.poi_value)
--- saaty/services/poi_time_latency.py
+++ saaty/services/poi_time_latency.py
@@ -16,18 +16,21 @@ def get_poi_latency_score(city_id, supplier_id, supplier_lng, supplier_lat, rece
     supplier_result = POISupplierTimeDifficulty.query \
         .filter(POISupplierTimeDifficulty.supplier_id == supplier_id,
                 POISupplierTimeDifficulty.supplier_lng == supplier_lng,
-                POISupplierTimeDifficulty.supplier_lat == supplier_lat) \
-        .all()
+                POISupplierTimeDifficulty.supplier_lat == supplier_lat,
+                POISupplierTimeDifficulty.city_id == city_id) \
+        .first()
     if supplier_result:
-        supplier_time_difficulty = supplier_result[0].poi_value
+        supplier_time_difficulty = supplier_result.poi_value
 
     # 获取送达成本难度系数
     receiver_result = POIReceiverTimeDifficulty.query \
         .filter(POIReceiverTimeDifficulty.receiver_lng == receiver_lng,
-                POIReceiverTimeDifficulty.receiver_lat == receiver_lat) \
-        .all()
+                POIReceiverTimeDifficulty.receiver_lat == receiver_lat,
+                POIReceiverTimeDifficulty.city_id == city_id) \
+        .first()
+
     if receiver_result:
-        receiver_time_difficulty = receiver_result[0].poi_value
+        receiver_time_difficulty = receiver_result.poi_value
 
     # 计算难度系数
     latency_param = app.config.get("POI_LATENCY_PARAM", {})
@@ -35,8 +38,4 @@ def get_poi_latency_score(city_id, supplier_id, supplier_lng, supplier_lat, rece
         latency_score = latency_param["alpha_1"]*supplier_time_difficulty + latency_param["alpha_2"]*receiver_time_difficulty
 
     return latency_score, supplier_time_difficulty, receiver_time_difficulty
-
-
-def get_poi_latency_ratio(latency_score):
-    pass
 
