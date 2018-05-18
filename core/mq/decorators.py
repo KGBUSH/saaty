c3025b9314a76f75686bae0d6e0f7ba0723ec06c
jonnynbu <jiangnbu@me.com>
2018-05-18 15:36:58 +0800

    增加dw_api到saaty_db数据迁移job
    摘要: 增加dw_api到saaty_db数据迁移job
    
    Differential 修订: http://phabricator.corp.imdada.cn/D3697

--- core/mq/decorators.py
+++ core/mq/decorators.py
@@ -2,13 +2,18 @@
 
 from core.tasks import Task
 
+__all__ = [
+    'task',
+]
 
-def task(consumer=None, producer=None, topic=None):
+
+def task(consumer=None, producer=None, topic=None, async=True):
     """
     消息队列任务装饰器
     :param consumer: 消费者客户端
     :param producer: 生产者客户端
     :param topic: topic / queue_name / exchange_name
+    :param async: 是否在异步线程池消费
     """
 
     def inner_task(func):
@@ -17,6 +22,7 @@ def task(consumer=None, producer=None, topic=None):
             consumer=consumer,
             producer=producer,
             topic=topic,
+            async=async,
         )
 
     return inner_task
