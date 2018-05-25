09759d5293a2f96268d61388663240e4da7a936d
jonnynbu <jiangnbu@me.com>
2018-05-25 18:44:30 +0800

    poi_time_latency 增加cache
    评审者: huyadong, zhuangxuekun, liuyiming
    
    Reviewed By: zhuangxuekun
    
    Differential 修订: http://phabricator.corp.imdada.cn/D3811

--- saaty/constants/cache_keys.py
+++ saaty/constants/cache_keys.py
@@ -2,7 +2,6 @@
 
 ALL_CITY_LIST = 'all_city_list'
 
-
 # 发货地取货时间开销
 CACHE_KEY_PICKUP_TIME_OVERHEAD = 'saaty_pickup_time_overhead_cache_key_' \
                                  '{cityId}_{supplierId}_{supplierLng}_' \
@@ -11,3 +10,11 @@ CACHE_KEY_PICKUP_TIME_OVERHEAD = 'saaty_pickup_time_overhead_cache_key_' \
 # 收货地取货时间开销
 CACHE_KEY_RECEIVER_TIME_OVERHEAD = 'saaty_receiver_time_overhead_cache_key_' \
                                    '{cityId}_{receiverLng}_{receiverLat}'
+
+# 商户取货难度系数
+SUPPLIER_TIME_DIFFICULTY = 'supplier_time_difficulty_{supplier_id}_' \
+                           '{supplier_lng}_{supplier_lat}_{city_id}'
+
+# POI 送达难度系数
+RECEIVER_TIME_DIFFICULTY = 'receiver_time_difficulty_{receiver_lng}_' \
+                           '{receiver_lat}_{city_id}'
--- saaty/services/poi_time_latency.py
+++ saaty/services/poi_time_latency.py
@@ -2,43 +2,133 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2018 xuekun.zhuang <zhuangxuekun@imdada.cn>
+
 from core import app
+from core import cache
+from saaty.constants import cache_keys
+from saaty.constants import cache_expire
 from saaty.models.poi_time_difficulty import POIReceiverTimeDifficulty
 from saaty.models.poi_time_difficulty import POISupplierTimeDifficulty
 
+__all__ = [
+    'get_poi_latency_score',
+    'get_poi_latency_delta',
+    'get_poi_latency_difficulty'
+]
 
-def get_poi_latency_difficulty(city_id, supplier_id, supplier_lng, supplier_lat, receiver_lng, receiver_lat):
+
+def read_supplier_time_difficulty(city_id, supplier_id, supplier_lng,
+                                  supplier_lat):
+    """
+    获取商户取货难度系数，优先使用cache数据
+    :param city_id:
+    :param supplier_id:
+    :param supplier_lng:
+    :param supplier_lat:
+    :return:
+    """
     supplier_time_difficulty = 0.0
+
+    # 从缓存获取
+    supplier_cache_key = cache_keys.SUPPLIER_TIME_DIFFICULTY.format(
+        supplier_id=supplier_id, supplier_lng=supplier_lng,
+        supplier_lat=supplier_lat, city_id=city_id,
+    )
+    supplier_time_cache = cache.get(supplier_cache_key)
+
+    if supplier_time_cache is not None:
+        supplier_time_difficulty = supplier_time_cache
+    else:
+        # 从数据库获取
+        supplier_result = POISupplierTimeDifficulty.query \
+            .filter(POISupplierTimeDifficulty.supplier_id == supplier_id,
+                    POISupplierTimeDifficulty.supplier_lng == supplier_lng,
+                    POISupplierTimeDifficulty.supplier_lat == supplier_lat,
+                    POISupplierTimeDifficulty.city_id == city_id) \
+            .first()
+        if supplier_result:
+            supplier_time_difficulty = supplier_result.poi_value
+
+        cache.set(supplier_cache_key, supplier_time_difficulty,
+                  cache_expire.CHCHE_EXPIRE_TIME_LONG)
+
+    return supplier_time_difficulty
+
+
+def read_poi_receiver_time_difficulty(city_id, receiver_lng, receiver_lat):
+    """
+    获取POI维度送达时间难度系数，优先使用缓存数据
+    :param city_id:
+    :param receiver_lng:
+    :param receiver_lat:
+    :return:
+    """
     receiver_time_difficulty = 0.0
 
-    # 获取取货成本难度系数
-    supplier_result = POISupplierTimeDifficulty.query \
-        .filter(POISupplierTimeDifficulty.supplier_id == supplier_id,
-                POISupplierTimeDifficulty.supplier_lng == supplier_lng,
-                POISupplierTimeDifficulty.supplier_lat == supplier_lat,
-                POISupplierTimeDifficulty.city_id == city_id) \
-        .first()
-    if supplier_result:
-        supplier_time_difficulty = supplier_result.poi_value
+    #从缓存中获取
+    receiver_cache_key = cache_keys.RECEIVER_TIME_DIFFICULTY.format(
+        receiver_lng=receiver_lng, receiver_lat=receiver_lat, city_id=city_id,
+    )
+    receiver_time_cache = cache.get(receiver_cache_key)
 
-    # 获取送达成本难度系数
-    receiver_result = POIReceiverTimeDifficulty.query \
-        .filter(POIReceiverTimeDifficulty.receiver_lng == receiver_lng,
-                POIReceiverTimeDifficulty.receiver_lat == receiver_lat,
-                POIReceiverTimeDifficulty.city_id == city_id) \
-        .first()
+    if receiver_time_cache is not None:
+        receiver_time_difficulty = receiver_time_cache
+    else:
+        # 从数据库获取
+        receiver_result = POIReceiverTimeDifficulty.query \
+            .filter(POIReceiverTimeDifficulty.receiver_lng == receiver_lng,
+                    POIReceiverTimeDifficulty.receiver_lat == receiver_lat,
+                    POIReceiverTimeDifficulty.city_id == city_id) \
+            .first()
 
-    if receiver_result:
-        receiver_time_difficulty = receiver_result.poi_value
+        if receiver_result:
+            receiver_time_difficulty = receiver_result.poi_value
+
+        cache.set(receiver_cache_key, receiver_time_difficulty,
+                  cache_expire.CHCHE_EXPIRE_TIME_LONG)
+
+    return receiver_time_difficulty
+
+
+def get_poi_latency_difficulty(city_id, supplier_id, supplier_lng, supplier_lat,
+                               receiver_lng, receiver_lat):
+    """
+    获取取货难度系数，送达难度系数
+    :param city_id:
+    :param supplier_id:
+    :param supplier_lng:
+    :param supplier_lat:
+    :param receiver_lng:
+    :param receiver_lat:
+    :return:
+    """
+    supplier_time_difficulty = read_supplier_time_difficulty(
+        city_id=city_id,
+        supplier_id=supplier_id,
+        supplier_lng=supplier_lng,
+        supplier_lat=supplier_lat
+    )
+
+    receiver_time_difficulty = read_poi_receiver_time_difficulty(
+        city_id=city_id,
+        receiver_lat=receiver_lat,
+        receiver_lng=receiver_lng
+    )
 
     return supplier_time_difficulty, receiver_time_difficulty
 
 
-def get_poi_latency_score(alpha_1, alpha_2, supplier_time_difficulty, receiver_time_difficulty):
+def get_poi_latency_score(alpha_1, alpha_2, supplier_time_difficulty,
+                          receiver_time_difficulty):
     latency_score = 0.0
 
     # 计算难度系数
     latency_score = alpha_1 * supplier_time_difficulty + alpha_2 * receiver_time_difficulty
+
+    if latency_score < 0.0:
+        latency_score = 0.0
+    if latency_score > 1.0:
+        latency_score = 1.0
 
     return latency_score
 
@@ -47,7 +137,8 @@ def get_poi_latency_delta(original_latency, dynamic_latency_ratio):
     min_latency_delta = 0
     # 延时上限=40min
     max_latency_delta = app.config.get("POI_LATENCY_MAX_LATENCY_DELTA", 2400)
-    dynamic_latency_delta = round((original_latency * dynamic_latency_ratio) / 300) * 300
+    dynamic_latency_delta = round(
+        (original_latency * dynamic_latency_ratio) / 300) * 300
 
     if dynamic_latency_delta <= min_latency_delta:
         dynamic_latency_delta = min_latency_delta
