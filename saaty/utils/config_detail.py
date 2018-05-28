f1e4dae3f6264e0e88f4e27fa8abd9ce10c2a693
刘昳明 <liuyiming@imdada.cn>
2018-05-28 11:30:34 +0800

    The method of getting the difficulty of supplier_id and the receiver_geohash was modified
    摘要: the method of getting the difficulty of supplier_id and the receiver_geohash was modified
    
    Differential 修订: http://phabricator.corp.imdada.cn/D3816

--- saaty/utils/config_detail.py
+++ saaty/utils/config_detail.py
@@ -2,15 +2,15 @@
 
 
 def get_config_detail(ab_test_flag):
-    control_flag = 1
-    latency_config_group = 100
+    items = ab_test_flag.strip().split('_')
+
+    control_flag = 1 if items[0] == 'con' else 0
+    latency_config_group = int(items[1])
+
     get_difficulty_method = 'm1'
 
-    items = ab_test_flag.strip().split('_')
     if len(items) > 2:
         try:
-            control_flag = 1 if items[0] == 'con' else 0
-            latency_config_group = int(items[1])
             get_difficulty_method = str(items[2])
         except:
             pass
