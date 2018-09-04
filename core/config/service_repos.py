aa0ac871ae2a5de390561ea0f7284ec617876138
刘昳明 <liuyiming@imdada.cn>
2018-09-04 15:12:27 +0800

    get poi_id via lat and lng

--- core/config/service_repos.py
+++ core/config/service_repos.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# delivery-center
-SERVICE_RPC_DELIVERY_CENTER = 'delivery-center'
+# 服务注册发现
+SERVICE_RPC_DELIVERY_CENTER = 'delivery-center'  # delivery-center
+SERVICE_RPC_HUBBLE_POI = 'hubble'  # hubble-poi
 
-# 配送服务
+# service
 ORDER_DETAIL_SINGLE = 'order.detail.single'
 ORDER_DETAIL_BATCH = 'order.detail.batch'
+LNG_LAT_POI_ID = 'latlng.poi.id'
+ADDRESS_POI_ID = 'address.poi.id'
+
 
 # 服务注册仓库配置
 SERVICE_REGISTRY_REPO = {
     SERVICE_RPC_DELIVERY_CENTER: {
         ORDER_DETAIL_SINGLE: '/order/detail/id',
         ORDER_DETAIL_BATCH: '/order/detail/ids'
+    },
+    SERVICE_RPC_HUBBLE_POI : {
+        LNG_LAT_POI_ID: '/v1/poi/get-info-by-latlng',
+        ADDRESS_POI_ID: '/v1/poi/get-info-by-poiname'
     }
 }
