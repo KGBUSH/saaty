83242349c342fbb36e6a33ab76a7a602a5b762bb
zhenfengjiang <zhenfengjiang@imdada.cn>
2019-11-21 14:24:38 +0800

    [add] 增加动态时效标签黑名单
    评审者: licai
    
    Differential 修订: http://phabricator.corp.imdada.cn/D16909

--- saaty/views/v1/poi_latency_ratio.py
+++ saaty/views/v1/poi_latency_ratio.py
@@ -7,7 +7,6 @@ import saaty.services.poi_time_latency as poi_latency_service
 from core import algoKafkaLogger
 from saaty.constants import kafka_event
 
-
 __all__ = [
     'POILatencyRatioView',
 ]
@@ -29,7 +28,8 @@ class POILatencyRatioView(JsonView):
 
         try:
             order_id = int(request.args.get('orderId', 0))  # 不必需
-            original_latency = int(request.args.get('originalLatency', 0))  # 单位：s，不必需
+            original_latency = int(
+                request.args.get('originalLatency', 0))  # 单位：s，不必需
             supplier_id = int(request.args['supplierId'])
             supplier_lng = str(request.args['supplierLng'])
             supplier_lat = str(request.args['supplierLat'])
@@ -38,7 +38,8 @@ class POILatencyRatioView(JsonView):
             receiver_lat = str(request.args['receiverLat'])
             label_ids = str(request.args['lableIDs'])
             # 运营在雨天会有额外的延时配置
-            heavy_weather_latency = int(request.args.get('heavyWeatherTime', 0))*60  # 单位：s，不必需
+            heavy_weather_latency = int(
+                request.args.get('heavyWeatherTime', 0)) * 60  # 单位：s，不必需
         except(TypeError, ValueError, KeyError):
             self.update_errors(self.error_messages['args_error'])
             return {}
@@ -49,41 +50,52 @@ class POILatencyRatioView(JsonView):
             'LatencyDelta': 0,
         }
 
-        # 优先检查人工收集 POI 延时
-        success, dynamic_latency_ratio, dynamic_latency_delta = \
-            poi_latency_service.get_artificial_poi_latency_info(
-                city_id=city_id,
-                order_id=order_id,
-                label_ids=label_ids,
-                receiver_lat=receiver_lat,
-                receiver_lng=receiver_lng,
-                original_latency=original_latency,
-                heavy_weather_latency=heavy_weather_latency
-            )
-        if success:
-            context = {
-                'LatencyRatio': dynamic_latency_ratio,
-                'LatencyDelta': dynamic_latency_delta,
-            }
-        elif app.config.get("POI_LATENCY_GLOBAL_SWITCH", 0):
-            # 困难POI延时
-            dynamic_latency_ratio, dynamic_latency_delta = poi_latency_service.get_poi_latency_view_result(
-                city_id=city_id,
-                supplier_id=supplier_id,
-                supplier_lat=supplier_lat,
-                supplier_lng=supplier_lng,
-                receiver_lat=receiver_lat,
-                receiver_lng=receiver_lng,
-                original_latency=original_latency,
-                order_id=order_id,
-                label_ids=label_ids,
-                heavy_weather_latency=heavy_weather_latency
-            )
-            context = {
-                'LatencyRatio': dynamic_latency_ratio,
-                'LatencyDelta': dynamic_latency_delta,
-            }
+        dynamic_time_enable = True
 
+        # 检查黑名单
+        if isinstance(label_ids, str):
+            label_list = map(lambda x: int(x), label_ids.strip().split(','))
+            label_black_list = app.config.get("ORDER_BLACK_LIST_LABEL", [])
+            blacklist_label_id = set(label_list).intersection(
+                set(label_black_list))
+            if blacklist_label_id:
+                dynamic_time_enable = False
+
+        if dynamic_time_enable:
+            # 优先检查人工收集 POI 延时
+            success, dynamic_latency_ratio, dynamic_latency_delta = \
+                poi_latency_service.get_artificial_poi_latency_info(
+                    city_id=city_id,
+                    order_id=order_id,
+                    label_ids=label_ids,
+                    receiver_lat=receiver_lat,
+                    receiver_lng=receiver_lng,
+                    original_latency=original_latency,
+                    heavy_weather_latency=heavy_weather_latency
+                )
+            if success:
+                context = {
+                    'LatencyRatio': dynamic_latency_ratio,
+                    'LatencyDelta': dynamic_latency_delta,
+                }
+            elif app.config.get("POI_LATENCY_GLOBAL_SWITCH", 0):
+                # 困难POI延时
+                dynamic_latency_ratio, dynamic_latency_delta = poi_latency_service.get_poi_latency_view_result(
+                    city_id=city_id,
+                    supplier_id=supplier_id,
+                    supplier_lat=supplier_lat,
+                    supplier_lng=supplier_lng,
+                    receiver_lat=receiver_lat,
+                    receiver_lng=receiver_lng,
+                    original_latency=original_latency,
+                    order_id=order_id,
+                    label_ids=label_ids,
+                    heavy_weather_latency=heavy_weather_latency
+                )
+                context = {
+                    'LatencyRatio': dynamic_latency_ratio,
+                    'LatencyDelta': dynamic_latency_delta,
+                }
 
         # 接口响应日志
         request_respons_info = {
@@ -99,6 +111,7 @@ class POILatencyRatioView(JsonView):
             "heavy_weather_latency": heavy_weather_latency,
             "response_context": context
         }
-        algoKafkaLogger.info(kafka_event.DYNAMIC_POI_LATENCY_ONLINE_CHECK, request_respons_info)
+        algoKafkaLogger.info(kafka_event.DYNAMIC_POI_LATENCY_ONLINE_CHECK,
+                             request_respons_info)
 
         return context
